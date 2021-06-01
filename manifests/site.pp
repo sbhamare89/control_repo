f3d2a1810585788fd09d1d04c45cc1ea34d1210e
sbhamare89 <80466696+sbhamare89@users.noreply.github.com>
2021-06-01 16:14:37 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,3 +4,11 @@ node default {
 node 'master.puppet.vm' {
     include role::master_server
 }
+
+node 'web.puppet.vm' {
+    include role::app_server
+}
+
+node 'db.puppet.vm' {
+    include role::db_server
+}
