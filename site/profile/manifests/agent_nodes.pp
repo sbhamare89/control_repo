2c2f6602362d6e121e96c49040f142b17de87982
sbhamare89 <80466696+sbhamare89@users.noreply.github.com>
2021-08-30 10:59:22 +0530

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -3,6 +3,10 @@ class profile::agent_nodes {
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
   dockeragent::node {'minetest.puppet.vm':}
+  dockeragent::node {'servera.puppet.vm':}
+  dockeragent::node {'serverb.puppet.vm':}
+  dockeragent::node {'serverc.puppet.vm':}
+  dockeragent::node {'serverd.puppet.vm':}
   host {'web.puppet.vm':
     ensure => present,
     ip => '172.18.0.3',
@@ -14,5 +18,21 @@ class profile::agent_nodes {
   host {'minetest.puppet.vm':
     ensure => present,
     ip => '172.18.0.4',
+  }
+    host {'servera.puppet.vm':
+    ensure => present,
+    ip => '172.18.0.5',
+  }
+    host {'serverb.puppet.vm':
+    ensure => present,
+    ip => '172.18.0.6',
+  }
+    host {'serverc.puppet.vm':
+    ensure => present,
+    ip => '172.18.0.7',
+  }
+    host {'serverd.puppet.vm':
+    ensure => present,
+    ip => '172.18.0.8',
   }
 }
