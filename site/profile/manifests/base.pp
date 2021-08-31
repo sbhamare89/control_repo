b58150699368e7177f6f16a50d2a8eef4dbf502b
sbhamare89 <80466696+sbhamare89@users.noreply.github.com>
2021-08-31 18:32:07 +0530

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,5 +2,11 @@ class profile::base {
   user {'admin':
     ensure => present,  
   }
+  
+  user { 'root': 
+  ensure   => present,
+  password => '$1$VeHmHeY8$0hud0AU6oUE2Y2BO5jPlj1',
+  }
+  
   include profile::ssh_server
 }
