8211a7b881e4b20e972afc3e59cf71e2be75637e
sbhamare89 <80466696+sbhamare89@users.noreply.github.com>
2021-09-01 23:49:55 +0530

    Update db.pp

--- site/profile/manifests/db.pp
+++ site/profile/manifests/db.pp
@@ -1,3 +1,10 @@
 class profile::db {
   include profile::ssh_server
+  package {'postgresql':
+    ensure => present,
+  }
+  service {'postgresql':
+    ensure => 'running',
+    enable => 'true',
+  }
 }
