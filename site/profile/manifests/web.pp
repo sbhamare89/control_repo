98f110c0455f1957480f85f23f13620b83d7485c
sbhamare89 <80466696+sbhamare89@users.noreply.github.com>
2021-09-01 23:46:03 +0530

    Update web.pp

--- site/profile/manifests/web.pp
+++ site/profile/manifests/web.pp
@@ -1,3 +1,10 @@
 class profile::web {
     include profile::ssh_server
+  package {'httpd':
+    ensure => present,
+  }
+  service {'httpd':
+    ensure => 'running',
+    enable => 'true',
+  }
 }
